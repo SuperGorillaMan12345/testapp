02f7307114bdd5279f1ad5b737e1d0faae98cf6c
SuperGorillaMan12345 <supergorillaman111@gmail.com>
2026-02-18 04:37:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My App</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Hello GitHub</h1>
+  <button onclick="alert('動いた！')">Click</button>
+</body>
+</html>
